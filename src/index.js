771a6a54c6affbfbff300c6fa6d475bbd28ae370
Tobin South <tobin.south@gmail.com>
2022-06-03 12:04:15 -0400

    Setting up basic template

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import App from './components/App';
+import Model from './components/pages/Model';
+import Models from './components/pages/Models';
+import HomePage from './components/pages/HomePage';
+import NotFound from './components/pages/NotFound';
+
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<App />}>
+        {/* Passed into Outlet in App */}
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/model/:creator/:name" element={<Model/>} />
+        <Route path="/models" element={<Models/>} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-);
+  );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
